ddc4775297a2a837618fbc4eabeba614e0a948b8
Ahad726 <atmahad726@gmail.com>
2021-04-21 15:55:17 +0600

    4A

---
+++ ProblemSolving/ProblemSolving/Watermelon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class Watermelon
+    {
+        static void Main(string[] args)
+        {
+            int watermelonWeight = Convert.ToInt32(Console.ReadLine());
+
+            if (watermelonWeight % 2 == 0 && watermelonWeight > 2)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
