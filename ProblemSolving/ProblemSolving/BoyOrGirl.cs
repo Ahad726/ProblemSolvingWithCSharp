16c7b07153a34da39127bf0cac965854eac7038f
Ahad726 <atmahad726@gmail.com>
2021-04-21 15:40:50 +0600

    Problem 236A

---
+++ ProblemSolving/ProblemSolving/BoyOrGirl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ProblemSolving
+{
+    class BoyOrGirl
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string distinctInput = string.Empty;            
+
+            foreach (var item in input)
+            {
+                if (!distinctInput.Contains(item))
+                {
+                    distinctInput += item;
+                }
+            }
+
+            int count = distinctInput.Length;
+
+            if(count % 2 == 0)
+            {
+                Console.WriteLine("CHAT WITH HER!");
+            }
+            else
+            {
+                Console.WriteLine("IGNORE HIM!");
+            }
+        }
+    }
+}
