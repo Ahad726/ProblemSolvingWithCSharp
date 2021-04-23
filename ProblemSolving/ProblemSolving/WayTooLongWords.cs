a960be0f21f74c48285f2b27addd54bf3b606ba9
Ahad726 <atmahad726@gmail.com>
2021-04-23 11:33:13 +0600

    Codeforces 71A

---
+++ ProblemSolving/ProblemSolving/WayTooLongWords.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class WayTooLongWords
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string inputString = Console.ReadLine();
+                int length = inputString.Length;
+
+                if (length > 10)
+                {
+                    inputString = inputString.Remove(1, length - 2);
+                    inputString = inputString.Insert(1, (length - 2).ToString());
+                }
+
+                Console.WriteLine(inputString);
+            }
+        }
+    }
+}
