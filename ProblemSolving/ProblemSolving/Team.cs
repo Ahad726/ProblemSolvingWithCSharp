3b9345b57c08bc54b760af32cf14dcb688fb2a3d
Ahad726 <atmahad726@gmail.com>
2021-04-24 13:41:18 +0600

    COdeforces 231A

---
+++ ProblemSolving/ProblemSolving/Team.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class Team
+    {
+        static void Main(string[] args)
+        {
+
+            int totalProblems = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+
+            for (int i = 0; i < totalProblems; i++)
+            {
+                var line = Console.ReadLine().Split(' ');
+
+                int petyaSolution = Convert.ToInt32(line[0]);
+                int vasyaSolution = Convert.ToInt32(line[1]);
+                int tonyaSolution = Convert.ToInt32(line[2]);
+
+                if (petyaSolution + vasyaSolution + tonyaSolution > 1)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+
+           
+        }
+    }
+}
