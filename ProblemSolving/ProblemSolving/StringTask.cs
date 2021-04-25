ae4b25696aaaed698e262b282fc5745508bc4dd3
Ahad726 <atmahad726@gmail.com>
2021-04-25 12:52:46 +0600

    Codeforces 118A

---
+++ ProblemSolving/ProblemSolving/StringTask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class StringTask
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().ToLower();
+
+            var vowels =new char[] { 'a', 'e', 'i', 'o', 'u', 'y' };
+
+            string result = string.Empty;
+
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!vowels.Contains(input[i]))
+                {
+                    result += "." + input[i];
+                }
+                              
+            }
+
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
