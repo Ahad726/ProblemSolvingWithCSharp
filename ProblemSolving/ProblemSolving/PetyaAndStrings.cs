3415e75aaf5fcbecf2d2926141c000a629e6c7ff
Ahad726 <atmahad726@gmail.com>
2021-04-25 14:40:32 +0600

    Codeforces 112A

---
+++ ProblemSolving/ProblemSolving/PetyaAndStrings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class PetyaAndStrings
+    {
+        static void Main(string[] args)
+        {
+            string firstStr = Console.ReadLine();
+            string secondStr = Console.ReadLine();
+
+            int result = String.Compare(firstStr, secondStr, true);
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
