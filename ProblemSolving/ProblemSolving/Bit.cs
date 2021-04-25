a508a78191f175908ecd06f40ddc47e51b55e4c2
Ahad726 <atmahad726@gmail.com>
2021-04-25 13:47:20 +0600

    Codeforces 282A

---
+++ ProblemSolving/ProblemSolving/Bit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class Bit
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int x = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                if (input.Contains("++"))
+                {
+                    x++;
+                }
+                else if (input.Contains("--"))
+                {
+                    x--;
+                }
+            }
+
+            Console.WriteLine(x);
+
+        }
+    }
+}
