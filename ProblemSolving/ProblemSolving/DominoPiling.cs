84c59569325de878b9e6df5d19fa18fa697dbbc6
Ahad726 <atmahad726@gmail.com>
2021-04-25 14:30:05 +0600

    Codeforces 50A

---
+++ ProblemSolving/ProblemSolving/DominoPiling.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class DominoPiling
+    {
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine().Split(' ');
+
+            int m = Convert.ToInt32(line[0]);
+            int n = Convert.ToInt32(line[1]);
+
+            var result = Math.Floor( (m *n) / 2.0);
+
+            Console.WriteLine(result);
+        }
+    }
+}
