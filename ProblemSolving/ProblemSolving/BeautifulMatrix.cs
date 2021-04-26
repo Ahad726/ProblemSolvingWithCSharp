df9ebf03ac5a0a4657a051c3f740e7bd64375a30
Ahad726 <atmahad726@gmail.com>
2021-04-26 12:32:00 +0600

    Codeforces 263A

---
+++ ProblemSolving/ProblemSolving/BeautifulMatrix.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class BeautifulMatrix
+    {
+        static void Main(string[] args)
+        {
+
+            var matrix = new int[5,5];
+            int row = 0;
+            int col = 0;
+
+
+
+            for (int i = 0; i <5; i++)
+            {
+                var arr = Console.ReadLine().Split(' ').Take(5).Select(int.Parse).ToArray();
+
+                for (int j = 0; j < 5; j++)
+                {
+                    matrix[i,j] = arr[j];
+
+                    if (matrix[i, j] == 1)
+                    {
+                        row = i;
+                        col = j;
+                    }
+
+                }
+            }
+
+
+            int count = Math.Abs(2 - row) + Math.Abs(2 - col);
+            Console.WriteLine(count);
+
+        }
+    }
+}
