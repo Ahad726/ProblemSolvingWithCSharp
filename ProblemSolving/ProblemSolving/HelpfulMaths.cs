a1cc07595de253efa1a11fd0f57c4a306de1d5c6
Ahad726 <atmahad726@gmail.com>
2021-04-26 13:35:23 +0600

    Codeorces 339A

---
+++ ProblemSolving/ProblemSolving/HelpfulMaths.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class HelpfulMaths
+    {
+        static void Main(string[] args)
+        {
+            var summands = Console.ReadLine().Split('+');
+
+            var lst = new List<int>();
+            foreach (var item in summands)
+            {
+                lst.Add(Convert.ToInt32(item));
+            }
+
+            lst.Sort();
+           string output = string.Join('+', lst);
+
+            Console.WriteLine(output);
+        }
+    }
+}
