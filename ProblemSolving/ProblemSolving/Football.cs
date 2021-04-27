a06d6c7a22c7f6f2e616130ef5d72061ffee961d
Ahad726 <atmahad726@gmail.com>
2021-04-27 14:48:21 +0600

    Codeforces 96A  Football

---
+++ ProblemSolving/ProblemSolving/Football.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class Football
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int temp = input[0];
+            int count = 1;
+
+            for (int i = 1; i < input.Length ; i++)
+            {
+                if (count > 6)
+                {
+                    break;
+                }
+                if (input[i] == temp)
+                {
+                    count++;
+                }
+                else
+                {
+                    temp = input[i];
+                    count = 1;
+                }
+            }
+
+            if (count > 6)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
