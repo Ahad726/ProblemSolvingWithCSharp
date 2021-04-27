5b762791abd6dfb687d95b9c0f3212fc816336ae
Ahad726 <atmahad726@gmail.com>
2021-04-27 13:51:48 +0600

    Codeforces 266A Stones on the table

---
+++ ProblemSolving/ProblemSolving/StonesOnTheTable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class StonesOnTheTable
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            var stones = Console.ReadLine();
+
+            int count = 0;
+            for (int i = 0; i < stones.Length-1; i++)
+            {
+                if (stones[i] == stones[i+1])
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
