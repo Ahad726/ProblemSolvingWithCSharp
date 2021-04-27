b003727126a9886e48c2e99328357b8441eca343
Ahad726 <atmahad726@gmail.com>
2021-04-27 13:23:03 +0600

    Codeforces 281A

---
+++ ProblemSolving/ProblemSolving/WordCapitalization.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class WordCapitalization
+    {
+        static void Main(string[] args)
+        {
+            var word = Console.ReadLine().ToCharArray();
+
+            if (!Char.IsUpper(word[0]))
+            {
+                var firstLetter = word[0] - 32;
+
+                word[0] = (char)firstLetter;
+            }
+
+            Console.WriteLine(word);
+        }
+    }
+}
