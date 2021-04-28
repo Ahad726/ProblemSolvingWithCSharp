f50af46c7bf40cff4f3507ce04c27251328d3868
Ahad726 <atmahad726@gmail.com>
2021-04-28 13:15:45 +0600

    Codeforces 546A Soldier and Bananas

---
+++ ProblemSolving/ProblemSolving/SoldierAndBananas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class SoldierAndBananas
+    {
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine().Split(' ');
+            int costOfBanana = Convert.ToInt32(line[0]);
+            int totalDollar = Convert.ToInt32(line[1]);
+            int totalBanana = Convert.ToInt32(line[2]);
+
+            int totalCost = 0;
+
+            for (int i = 1; i <= totalBanana; i++)
+            {
+                totalCost += i * costOfBanana;
+            }
+
+
+            int borrow = totalCost > totalDollar ? (totalCost - totalDollar) : 0;
+
+            Console.WriteLine(borrow);
+
+        }
+    }
+}
