db25190b6467ee5613792efd6920708e814212fe
Ahad726 <atmahad726@gmail.com>
2021-04-28 15:06:34 +0600

    Codeforces 791A Bear and Big Brother

---
+++ ProblemSolving/ProblemSolving/BearAndBigBrother.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class BearAndBigBrother
+    {
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine().Split(' ');
+            int limakWeight = Convert.ToInt32(line[0]);
+            int bobWeight = Convert.ToInt32(line[1]);
+
+            int yearCount = 0;
+
+            while (limakWeight <= bobWeight)
+            {
+                limakWeight = 3 * limakWeight;
+                bobWeight = 2 * bobWeight;
+                yearCount++;
+            }
+
+            Console.WriteLine(yearCount);
+
+        }
+    }
+}
