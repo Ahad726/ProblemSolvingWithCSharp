3b813a5461288b4f64e74607679419efcd257173
Ahad726 <atmahad726@gmail.com>
2021-04-30 16:27:51 +0600

    Codeforces 617A Elephant

---
+++ ProblemSolving/ProblemSolving/Elephant.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class Elephant
+    {
+        static void Main(string[] args)
+        {
+            var n = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            int i = 5;
+            while (n > 0)
+            {
+                if (n < 5)
+                {
+                    n = n - i;
+                    i--;
+                    count++;
+                }
+                else
+                {
+                    count += (n / 5); 
+                    n = n % 5;
+                }
+                
+            }
+
+            Console.WriteLine(count);
+
+        }
+    }
+}
