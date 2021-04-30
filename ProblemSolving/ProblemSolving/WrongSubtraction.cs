8c9c3430ff3e5add74c3fcde0e32e7c0c5a15947
Ahad726 <atmahad726@gmail.com>
2021-04-30 11:24:16 +0600

    Codeforces  977A Wrong Subtraction

---
+++ ProblemSolving/ProblemSolving/WrongSubtraction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class WrongSubtraction
+    {
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine().Split(' ');
+            var n = Convert.ToInt64(line[0]);
+            var k = Convert.ToInt32(line[1]);
+
+            for (int i = 0; i < k; i++)
+            {
+                if (n % 10 == 0)
+                {
+                    n = n / 10;
+                }
+                else
+                {
+                    n -= 1;
+                }
+            }
+            Console.WriteLine(n);
+        }
+    }
+}
