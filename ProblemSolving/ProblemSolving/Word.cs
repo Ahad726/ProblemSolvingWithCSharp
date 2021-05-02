d5a3047b074409d78a3505b22ebf7b3f19e12567
Ahad726 <atmahad726@gmail.com>
2021-05-02 22:59:25 +0600

    Codeforces 59A Word

---
+++ ProblemSolving/ProblemSolving/Word.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class Word
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int lowerCount = 0;
+            int upperCount = 0;
+
+            foreach (var letter in word)
+            {
+                if (Char.IsLower(letter))
+                {
+                    lowerCount++;
+                }
+            }
+
+            upperCount = word.Length - lowerCount;
+
+            if (lowerCount >= upperCount)
+            {
+                Console.WriteLine(word.ToLower());
+            }
+            else
+            {
+                Console.WriteLine(word.ToUpper());
+            }
+        }
+    }
+}
