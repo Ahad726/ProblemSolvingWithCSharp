fc1cd43e81964212ae4b62a5041ff83b3e00a7d3
Ahad726 <atmahad726@gmail.com>
2021-05-03 17:40:59 +0600

    Codeforces 110A Nearly Lucky Number

---
+++ ProblemSolving/ProblemSolving/NearlyLuckyNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class NearlyLuckyNumber
+    {
+        static void Main(string[] args)
+        {
+
+           var nmbr =Console.ReadLine();
+            int count = 0;
+            foreach (var item in nmbr)
+            {
+                
+                if (item == '4' || item == '7')
+                {
+                    count++;
+                }
+            }
+
+            if (count == 4 || count == 7)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
