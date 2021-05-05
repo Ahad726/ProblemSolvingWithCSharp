8952ebc1d97c1fea0a859a66ba563c4f51593e95
Ahad726 <atmahad726@gmail.com>
2021-05-05 23:13:11 +0600

    Codechef FLOW001

---
+++ ProblemSolving/ProblemSolving/AddTwoNumbers .cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class AddTwoNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine().Split(' ');
+                int a = Convert.ToInt32(line[0]);
+                int b = Convert.ToInt32(line[1]);
+
+                Console.WriteLine(a+b);
+            }
+        }
+    }
+}
---
+++ ProblemSolving/ProblemSolving/ChatRoom.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class ChatRoom
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            int wordLength = word.Length;
+
+            int hIndex = word.IndexOf('h');
+
+            int eIndex = word.IndexOf('e', hIndex);
+
+            int lindex = word.IndexOf('l', eIndex);
+
+            int scndLindex = word.IndexOf('l', lindex);
+
+            int oIndex = word.IndexOf('o', scndLindex);
+
+            if (hIndex > 0 && eIndex > 0 && lindex > 0 && scndLindex > 0 && oIndex >0 )
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+           
+        }
+    }
+}
