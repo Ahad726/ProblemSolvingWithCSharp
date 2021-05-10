96abe1df0c2a4917761a08f08aec3119e6ba5bef
Ahad726 <atmahad726@gmail.com>
2021-05-10 14:38:47 +0600

    Codeforcs Translation

---
+++ ProblemSolving/ProblemSolving/Translation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class Translation
+    {
+        static void Main(string[] args)
+        {
+            var word = Console.ReadLine().ToCharArray();
+            string reversedWord = Console.ReadLine();
+
+            Array.Reverse(word);
+            string trWord = string.Join("",word);
+
+
+            if (trWord == reversedWord)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
