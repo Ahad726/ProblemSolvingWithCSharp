f66215d7f0e33382ab1818129ba4f6eb7260a118
Ahad726 <atmahad726@gmail.com>
2021-05-11 23:01:58 +0600

    Codeforces 734A Anton and Danik

---
+++ ProblemSolving/ProblemSolving/AntonAndDanik.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class AntonAndDanik
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string match = Console.ReadLine();
+            int aktonCount = 0;
+            int danikCount = 0;
+
+            foreach (var letter in match)
+            {
+                if (letter == 'A')
+                {
+                    aktonCount++;
+                }
+            }
+
+            danikCount = n - aktonCount;
+
+            if (aktonCount > danikCount)
+            {
+                Console.WriteLine("Anton");
+            }
+            else if(aktonCount < danikCount)
+            {
+                Console.WriteLine("Danik");
+            }
+            else
+            {
+                Console.WriteLine("Friendship");
+            }
+        }
+    }
+}
