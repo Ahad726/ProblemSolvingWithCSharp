4300e31d079a42b8c31c5d0700b089d1a0de7969
Ahad726 <atmahad726@gmail.com>
2021-05-13 14:21:45 +0600

    Codeforces 133A HQ9+

---
+++ ProblemSolving/ProblemSolving/HQ9.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class HQ9
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            bool isMatch = false;
+
+            foreach (var ch in line)
+            {
+                if (ch == 'H' || ch == 'Q' || ch =='9' )
+                {
+                    isMatch = true;
+                }
+
+            }
+
+            string result = isMatch == true ? "YES" : "NO";
+            Console.WriteLine(result);
+        }
+    }
+}
