7c1332af91019c50d41550758bb10c67c5a05477
Ahad726 <atmahad726@gmail.com>
2021-05-23 22:00:25 +0600

    Potion_making

---
+++ ProblemSolving/ProblemSolving/PermutationSort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class PermutationSort
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int count = 0;
+                int d = Convert.ToInt32(Console.ReadLine());
+
+                var arr = Console.ReadLine().Split(' ').Take(d).Select(int.Parse).ToArray();
+
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        count++;
+                    }
+                }
+
+                Console.Write(count);
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ ProblemSolving/ProblemSolving/Potion-making.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class Potion_making
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int essence = Convert.ToInt32(Console.ReadLine());
+
+                if (100 % essence == 0)
+                {
+                    Console.WriteLine(100/essence);
+                }
+                else
+                {
+                    Console.WriteLine(100);
+                }
+            }
+        }
+    }
+}
