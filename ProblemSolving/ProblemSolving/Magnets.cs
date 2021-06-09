120efefedcea336495995caa35bf063d8b52f241
Ahad726 <atmahad726@gmail.com>
2021-06-09 21:33:31 +0600

    Codeforces 344A Magnets

---
+++ ProblemSolving/ProblemSolving/Magnets.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    public class Magnets
+    {
+        static void Main(string[] args)
+        {
+            int magnetNumber = Convert.ToInt32(Console.ReadLine());
+            int count = 1;
+            string firstpair = string.Empty;
+            for (int i = 0; i < magnetNumber; i++)
+            {
+                if (i == 0)
+                {
+                    firstpair = Console.ReadLine();
+                    continue;
+                }
+                var otherPair = Console.ReadLine();
+                if (firstpair[1] == otherPair[0])
+                {
+                    count++;
+                }
+                firstpair = otherPair;
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ ProblemSolving/ProblemSolving/OmkarAndBadStory.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class OmkarAndBadStory
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int k = 0; k < n; k++)
+            {
+                var arraySize = Convert.ToInt32(Console.ReadLine());
+                var arrayA = Console.ReadLine().Split(' ').Take(arraySize).Select(int.Parse).ToList();
+                int addCount = 0;
+                bool isNice = false;
+
+                for (int i = 0; i < arrayA.Count - 1; i++)
+                {
+                    for (int j = i+1; j < arrayA.Count; j++)
+                    {
+                        int result;
+                        if (arrayA[i] > arrayA[j])
+                        {
+                            result = arrayA[i] - arrayA[j];
+                        }
+                        else
+                        {
+                            result = arrayA[j] - arrayA[i];
+                        }
+
+                        if (!arrayA.Contains(result))
+                        {
+                            i = 0;
+                            j = i;
+                            arrayA.Add(result);
+                            isNice = true;
+                            addCount++;
+                        }
+                    }
+
+                    if (i == arrayA.Count -1)
+                    {
+                        break;
+                    }
+
+                    if (addCount == 300)
+                    {
+                        break;
+                    }
+                }
+
+                if (isNice)
+                {
+                    Console.WriteLine("YES");
+                    Console.WriteLine(arrayA.Count);
+                    for (int m = 0; m < arrayA.Count; m++)
+                    {
+                        Console.Write($"{arrayA[m]} ");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
