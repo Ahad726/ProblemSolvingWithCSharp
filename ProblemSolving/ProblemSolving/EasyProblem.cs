71bed597b81c6130afac27d35765a0c9cfc91ad9
Ahad726 <atmahad726@gmail.com>
2021-06-09 21:42:22 +0600

    Codeforces 1030A In Search of an Easy Problem

---
+++ ProblemSolving/ProblemSolving/EasyProblem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    public class EasyProblem
+    {
+        static void Main(string[] args)
+        {
+            int totalPeople = Convert.ToInt32(Console.ReadLine());
+            var opinion = Console.ReadLine();
+
+            if (opinion.Contains('1'))
+            {
+                Console.WriteLine("HARD");
+            }
+            else
+            {
+                Console.WriteLine("EASY");
+            }
+        }
+    }
+}
