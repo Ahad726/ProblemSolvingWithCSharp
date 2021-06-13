b0737a1449ebbe473ecff3b4ac8a6dbd84142af1
Ahad726 <atmahad726@gmail.com>
2021-06-13 06:01:53 +0600

    Codeforces 318A Even Odds

---
+++ ProblemSolving/ProblemSolving/EvenOdds.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    public class EvenOdds
+    {
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine().Split(' ');
+            var n = Convert.ToInt64(line[0]);
+            var k = Convert.ToInt64(line[1]);
+
+            var middleNumberPosition = n % 2 == 0 ? n / 2 : (n / 2) + 1;
+
+            if (k > middleNumberPosition)
+            {
+                // kth number is odd
+                var diffWithKAndMid = k - middleNumberPosition;
+                var diff = middleNumberPosition - diffWithKAndMid;
+                var kthNumber = k - diff;
+                Console.WriteLine(kthNumber);
+
+            }
+            else
+            {
+                var kthNumber = k + (k - 1);
+                Console.WriteLine(kthNumber);
+            }
+        }
+    }
+}
---
+++ ProblemSolving/ProblemSolving/NumberOfPairs.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class NumberOfPairs
+    {
+        static void Main(string[] args)
+        {
+            int testCase = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < testCase; i++)
+            {
+                var line = Console.ReadLine().Split(' ');
+                var n = Convert.ToInt32(line[0]);
+                var leftBound = Convert.ToInt32(line[1]);
+                var rightBound = Convert.ToInt32(line[2]);
+
+                var numbersArray = Console.ReadLine().Split(' ').Take(n).Select(Int64.Parse).ToArray();
+
+                Array.Sort(numbersArray);
+
+                int pairCount = 0;
+
+                for (int j = n - 1; j >= 0; j--)
+                {
+                    for (int k = j-1; k >= 0;  k--)
+                    {
+                        var number = numbersArray[j] + numbersArray[k];
+
+                        if (leftBound <= number && number <= rightBound)
+                        {
+                            pairCount++;
+                        }
+                        else if(leftBound > number)
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                Console.WriteLine(pairCount);
+
+            }
+        }
+    }
+}
