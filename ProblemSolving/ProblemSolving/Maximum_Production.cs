a8223b834a26e1b265e6b4bdfcb813a13cbb5da3
Ahad726 <atmahad726@gmail.com>
2021-07-02 18:18:24 +0600

    CodeChef July challenge problem 1

---
+++ ProblemSolving/ProblemSolving/Maximum_Production.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class Maximum_Production
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine().Split(' ');
+                var d = Convert.ToInt32(line[0]);
+                var x = Convert.ToInt32(line[1]);
+                var y = Convert.ToInt32(line[2]);
+                var z = Convert.ToInt32(line[3]);
+
+                var xUnitOfWork = 7 * x;
+
+                var yUnitOfWork = d * y + (7 - d) * z;
+
+                Console.WriteLine(Math.Max(xUnitOfWork,yUnitOfWork));
+            }
+        }
+    }
+}
