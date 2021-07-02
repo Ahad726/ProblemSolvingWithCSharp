a08d110af2dc72bcb5202a9ef08d38009ee01af9
Ahad726 <atmahad726@gmail.com>
2021-07-02 20:19:53 +0600

    CodeChef July,21 Problem 2

---
+++ ProblemSolving/ProblemSolving/Relativity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class Relativity
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine().Split(' ');
+                var g = Convert.ToInt32(line[0]);
+                var c = Convert.ToInt32(line[1]);
+
+                var result = (c / 2) * (c / g);
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
