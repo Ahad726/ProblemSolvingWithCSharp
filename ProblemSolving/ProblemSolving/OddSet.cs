4e9ca327af0c3b7105be8aafb0fa257b6190ffeb
Ahad726 <atmahad726@gmail.com>
2021-07-05 18:07:31 +0600

    Codeforces 729(Div 2) Problem 1

---
+++ ProblemSolving/ProblemSolving/OddSet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class OddSet
+    {
+        static void Main(string[] args)
+        {
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var totalnmbr = Convert.ToInt32(Console.ReadLine());
+                var nmbrArray = Console.ReadLine().Split(' ').Take(totalnmbr * 2).Select(int.Parse).ToArray();
+
+                int evencount = 0;
+                int oddcount = 0;
+
+                foreach (var nmbr in nmbrArray)
+                {
+                    if (nmbr % 2 == 0)
+                    {
+                        evencount++;
+                    }
+                    else
+                    {
+                        oddcount++;
+                    }
+                }
+
+                var result = evencount == oddcount ? "Yes" : "No";
+                Console.WriteLine(result);
+            }
+
+        }
+    }
+}
---
+++ ProblemSolving/ProblemSolving/OptimalDenomination.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class OptimalDenomination
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var totalEmp = Convert.ToInt32(Console.ReadLine());
+                var salaryArray = Console.ReadLine().Split(' ').Take(totalEmp).Select(int.Parse).ToArray();
+                var min = salaryArray.Min();
+                var max = salaryArray.Max();
+
+                bool havingOdd = false;
+
+                foreach (var s in salaryArray)
+                {
+                    if (s % 2 != 0)
+                    {
+                        havingOdd = true;
+                    }
+                }
+                Array.Sort(salaryArray);
+
+                if (havingOdd)
+                {
+                    for (int j = salaryArray.Length - 1; j >= 0; j--)
+                    {
+                        if (salaryArray[j] == max)
+                        {
+                            salaryArray[j] = 1;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    int count = 0;
+                    foreach (var s in salaryArray)
+                    {
+                        count += s / 1;
+                    }
+
+                    Console.WriteLine(count);
+                }
+                else
+                {
+                    for (int j = salaryArray.Length - 1; j >= 0; j--)
+                    {
+                        if (salaryArray[j] == max)
+                        {
+                            salaryArray[j] = min;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    int count = 0;
+                    foreach (var s in salaryArray)
+                    {
+                        count += s / min;
+                    }
+
+                    Console.WriteLine(count);
+                }
+
+              
+            }
+        }
+    }
+}
