09d51f27beeb260995d3d7e36f1d90b756fc676d
Ahad726 <atmahad726@gmail.com>
2021-07-31 05:56:48 +0600

    Codeforces Round 112 A

---
+++ ProblemSolving/ProblemSolving/PizzaForces.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ProblemSolving
+{
+    public class PizzaForces
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (int p = 0; p < t; p++)
+            {
+                var n = Convert.ToInt64(Console.ReadLine());
+
+                if (n <= 6)
+                {
+                    Console.WriteLine(15);
+                }
+                else
+                {
+                    long divResult =(long)Math.Ceiling((decimal)n / 2);
+                    Console.WriteLine(divResult*5);
+                }
+            }
+        }
+
+      
+    }
+}
---
+++ ProblemSolving/ProblemSolving/Problem_B.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ProblemSolving
+{
+    public class Problem_B
+    {
+        //static void Main(string[] args)
+        //{
+        //    int t = Convert.ToInt32(Console.ReadLine());
+
+        //    for (int p = 0; p < t; p++)
+        //    {
+        //        var arrayLength = Convert.ToInt32(Console.ReadLine());
+        //        var nmbrArray = Console.ReadLine().Split(' ').Take(arrayLength).Select(long.Parse).ToArray();
+
+
+
+
+        //    }
+        //}
+    }
+}
