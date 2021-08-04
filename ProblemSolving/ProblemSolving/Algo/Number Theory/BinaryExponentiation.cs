c6219bbf9c7318dee56846d59d9667d038a816f8
Ahad726 <atmahad726@gmail.com>
2021-08-04 21:05:49 +0600

    Codechef PRB01 PrimalityTest

---
+++ ProblemSolving/ProblemSolving/Algo/Number Theory/BinaryExponentiation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving.Algo.Number_Theory
+{
+    // finding the solution of a^n in O(log(n)) time complexity
+    public class BinaryExponentiation
+    {
+        static void Main(string[] args)
+        {
+            var t = Convert.ToInt32(Console.ReadLine());
+
+            for (int tstCase = 0; tstCase < t; tstCase++)
+            {
+                var line = Console.ReadLine().Split(' ');
+                var b = Convert.ToInt64(line[0]);
+                var power = Convert.ToInt64(line[1]);
+
+                var result = CalculatePower(b, power);
+                Console.WriteLine($"{b}^{power} = {result}");
+            }
+        }
+
+        static long CalculatePower(long b, long power)
+        {
+            long res = 1;
+
+            while (power > 0)
+            {
+                if (power % 2 != 0)
+                {
+                    res *= b;
+                    power--;
+                }
+                b *= b;
+                power /= 2;
+            }
+            return res;
+        }
+    }
+}
---
+++ ProblemSolving/ProblemSolving/Algo/Number Theory/PrimalityTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving.Algo.Number_Theory
+{
+    public class PrimalityTest
+    {
+        static void Main(string[] args)
+        {
+            var t = Convert.ToInt32(Console.ReadLine());
+
+            for (int tstCase = 0; tstCase < t; tstCase++)
+            {
+                var b = Convert.ToInt64(Console.ReadLine());
+                Console.WriteLine(IsPrime(b));
+            }
+        }
+
+        static bool IsPrime(long n)
+        {
+            if (n ==1)
+            {
+                return false;
+            }
+            for (int i = 2; i*i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+    }
+}
