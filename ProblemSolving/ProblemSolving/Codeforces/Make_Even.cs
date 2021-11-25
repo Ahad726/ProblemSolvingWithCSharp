c6f54d487bef4d05a24bc92f72a183f396643f37
Ahad726 <atmahad726@gmail.com>
2021-11-25 23:25:12 +0600

    Codeforces Round #756

---
+++ ProblemSolving/ProblemSolving/Codeforces/Make_Even.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving.Codeforces
+{
+    class Make_Even
+    {
+        static void Main(string[] args)
+        {
+            int testCase = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < testCase; i++)
+            {
+                var number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(EvenMaker(number));
+
+            }
+        }
+
+        static int EvenMaker(int number)
+        {
+            var numberLength = number.ToString().Length;
+
+            int divisor = 1;
+            for (int i = 1; i < numberLength; i++)
+            {
+                divisor *= 10;
+            }
+
+            int firstDigit = number / divisor;
+
+
+            if (number % 2 == 0)
+            {
+                return 0;
+            }
+
+            else if(firstDigit % 2 == 0)
+            {
+                return 1;
+            }
+
+            while (number > 0)
+            {
+                var numDiv = number / divisor;
+                number = number % divisor;
+                divisor /= 10;;
+
+                if (numDiv % 2 == 0)
+                {
+                    return 2;
+                }
+            }
+
+            return -1;
+
+        }
+    }
+}
---
+++ ProblemSolving/ProblemSolving/Codeforces/Team_Composition.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving.Codeforces
+{
+    class Team_Composition
+    {
+        static void Main(string[] args)
+        {
+            int testCase = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < testCase; i++)
+            {
+                var line = Console.ReadLine().Split(' ');
+                var numOfProg = Convert.ToInt32(line[0]);
+                var numOfMathm = Convert.ToInt32(line[1]);
+                Console.WriteLine(TeamComposer(numOfProg,numOfMathm));
+
+            }
+        }
+
+        static int TeamComposer(int numOfProg, int numOfMathm)
+        {
+            if (numOfMathm == 0 || numOfProg == 0)
+            {
+                return 0;
+            }
+
+            int totalTeamCount = (numOfProg + numOfMathm) / 4;
+
+            if (numOfProg > numOfMathm)
+            {
+                numOfProg = numOfMathm;
+            }
+            else
+            {
+                numOfMathm = numOfProg;
+            }
+
+            if (totalTeamCount > numOfProg)
+            {
+                return numOfProg;
+            }
+            else
+            {
+                return totalTeamCount;
+            }
+        }
+    }
+}
