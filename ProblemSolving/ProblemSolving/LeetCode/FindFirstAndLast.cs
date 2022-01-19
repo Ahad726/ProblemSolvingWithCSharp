a83e7fe91448c61d38d6a8e707905f041da4051f
Ahad726 <atmahad726@gmail.com>
2022-01-20 01:24:35 +0600

    leetcode problem solved

---
+++ ProblemSolving/ProblemSolving/LeetCode/FindFirstAndLast.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving.LeetCode
+{
+    public class FindFirstAndLast
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 5, 7, 7, 8, 8, 10 };
+            int target = 6;
+
+            var rslt = SearchRange(arr, target);
+
+            Console.WriteLine($"[{rslt[0]},{rslt[1]}]");
+        }
+
+        public static int[] SearchRange(int[] nums, int target)
+        {
+            int[] result = { -1, -1 };
+
+            result[0] = search(nums, target, findStartIndex: true);
+            if (result[0] != -1)
+            {
+                result[1] = search(nums, target, findStartIndex: false);
+            }
+
+            return result;
+        }
+
+        public static int search(int[] nums, int target, bool findStartIndex)
+        {
+            int start = 0;
+            int end = nums.Length - 1;
+            int ans = -1;
+
+            while (start <= end)
+            {
+                int midIndex = start + (end - start) / 2;
+                int mid = nums[midIndex];
+
+                if (mid < target)
+                {
+                    start = midIndex + 1;
+                }
+                else if (mid > target)
+                {
+                    end = midIndex - 1;
+                }
+                else
+                {
+
+                    ans = midIndex;
+
+                    if (findStartIndex)
+                    {
+                        end = midIndex - 1;
+                    }
+                    else
+                    {
+                        start = midIndex + 1;
+                    }
+
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ ProblemSolving/ProblemSolving/LeetCode/FindPositionInfiniteArray.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving.LeetCode
+{
+    internal class FindPositionInfiniteArray
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {3, 5, 7, 9, 10, 90, 100, 130,140, 160, 170,180,190,199,200,201,205,206,208,210,220,230,235,245,255};
+            Console.WriteLine(FindIndex(arr,100));
+        }
+
+        static int FindIndex(int[] arr, int target)
+        {
+            // first find the range
+            // start with box range of size 2
+            int start = 0;
+            int end = 1;
+
+            // condition for target lie in the range
+            while (target > arr[end])
+            {
+                int newStart = end + 1;
+
+                // double the box size
+                end = end + (end - start + 1) * 2;
+                start = newStart;
+            }
+
+            return BinarySearcher(arr,target,start, end);
+
+        }
+
+        static int BinarySearcher(int[] arr, int target, int start, int end)
+        {
+
+            while (start <= end)
+            {
+                // int arrMid = (start + end) / 2; might be possible that (start+end) exceed the int capacity
+
+                int arrMid = start + (end - start) / 2;
+                int mid = arr[arrMid];
+
+                if (mid < target)
+                {
+                    start = arrMid + 1;
+                }
+                else if (mid > target)
+                {
+                    end = arrMid - 1;
+                }
+                else
+                {
+                    return arrMid;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
