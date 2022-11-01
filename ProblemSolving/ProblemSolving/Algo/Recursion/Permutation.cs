a2a454cbfb4dfbfbe105955da968bce91f3cd085
Ahad726 <atmahad726@gmail.com>
2022-11-01 16:49:40 +0600

    PERMUTAION COUNT

--- ProblemSolving/ProblemSolving/Algo/Recursion/Permutation.cs
+++ ProblemSolving/ProblemSolving/Algo/Recursion/Permutation.cs
@@ -8,7 +8,8 @@ namespace ProblemSolving.Algo.Recursion
     {
         static void Main(string[] args)
         {
-            FindPermutation("", "ab");
+            //FindPermutation("", "ab");
+            Console.WriteLine(CountPermutation("", "abc"));
         }
 
         static void FindPermutation(string processed, string unProcessed)
@@ -28,6 +29,27 @@ namespace ProblemSolving.Algo.Recursion
                 FindPermutation(firstPart + ch + secondPart, unProcessed.Substring(1));
 
             }
+        }
+
+        static int CountPermutation(string processed, string unProcessed)
+        {
+
+            if (string.IsNullOrEmpty(unProcessed))
+            {
+                Console.WriteLine(processed);
+                return 1;
+            }
+            int count = 0;
+            var ch = unProcessed[0];
+
+            for (int i = 0; i <= processed.Length; i++)
+            {
+                var firstPart = processed.Substring(0, i);
+                var secondPart = processed.Substring(i, processed.Length - firstPart.Length);
+                count += CountPermutation(firstPart + ch + secondPart, unProcessed.Substring(1));
+            }
+
+            return count;
         }
     }
 }
