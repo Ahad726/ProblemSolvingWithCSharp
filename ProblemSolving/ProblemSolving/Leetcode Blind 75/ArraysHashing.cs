8510fff321792f62e4424b72d55164549c7830a1
Ahad <atmahad726@gmail.com>
2023-01-24 23:24:08 +0600

    IsAnagram.

--- ProblemSolving/ProblemSolving/Leetcode Blind 75/ArraysHashing.cs
+++ ProblemSolving/ProblemSolving/Leetcode Blind 75/ArraysHashing.cs
@@ -17,29 +17,6 @@ namespace ProblemSolving.Leetcode_Blind_75
             Console.WriteLine(isAnagram);
         }
 
-
-        //public static bool IsAnagram(string s, string t)
-        //{
-        //    if (s.Length != t.Length) return false;
-        //    if (s == t) return true;
-
-        //    Dictionary<char, int> sCounts = new Dictionary<char, int>();
-        //    Dictionary<char, int> tCounts = new Dictionary<char, int>();
-
-        //    for (int i = 0; i < s.Length; i++)
-        //    {
-        //        sCounts[s[i]] = 1 + (sCounts.ContainsKey(s[i]) ? sCounts[s[i]] : 0);
-        //        tCounts[t[i]] = 1 + (tCounts.ContainsKey(t[i]) ? tCounts[t[i]] : 0);
-        //    }
-
-        //    foreach (char c in sCounts.Keys)
-        //    {
-        //        int tCount = tCounts.ContainsKey(c) ? tCounts[c] : 0;
-        //        if (sCounts[c] != tCount) return false;
-        //    }
-        //    return true;
-        //}
-
         public static bool IsAnagram(string s, string t)
         {
             var sdict = new Dictionary<char, int>();
