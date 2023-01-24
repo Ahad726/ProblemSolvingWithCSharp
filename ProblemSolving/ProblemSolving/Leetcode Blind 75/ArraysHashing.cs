f038f1f0f267779c45ad84f9760a8b3a18509896
Ahad <atmahad726@gmail.com>
2023-01-24 23:23:32 +0600

    IsAnagram LeetCode

--- ProblemSolving/ProblemSolving/Leetcode Blind 75/ArraysHashing.cs
+++ ProblemSolving/ProblemSolving/Leetcode Blind 75/ArraysHashing.cs
@@ -8,13 +8,65 @@ namespace ProblemSolving.Leetcode_Blind_75
     {
         static void Main(string[] args)
         {
-            var output = TwoSum(new int[] { 3,2,4 }, 6);
+            //var output = TwoSum(new int[] { 3,2,4 }, 6);
 
-            Console.WriteLine(String.Join(',', output));
+            //Console.WriteLine(String.Join(',', output));
+
+            var isAnagram = IsAnagram("anagram", "nagaram");
+            //var isAnagram = IsAnagram("Rat", "cat");
+            Console.WriteLine(isAnagram);
         }
 
 
+        //public static bool IsAnagram(string s, string t)
+        //{
+        //    if (s.Length != t.Length) return false;
+        //    if (s == t) return true;
 
+        //    Dictionary<char, int> sCounts = new Dictionary<char, int>();
+        //    Dictionary<char, int> tCounts = new Dictionary<char, int>();
+
+        //    for (int i = 0; i < s.Length; i++)
+        //    {
+        //        sCounts[s[i]] = 1 + (sCounts.ContainsKey(s[i]) ? sCounts[s[i]] : 0);
+        //        tCounts[t[i]] = 1 + (tCounts.ContainsKey(t[i]) ? tCounts[t[i]] : 0);
+        //    }
+
+        //    foreach (char c in sCounts.Keys)
+        //    {
+        //        int tCount = tCounts.ContainsKey(c) ? tCounts[c] : 0;
+        //        if (sCounts[c] != tCount) return false;
+        //    }
+        //    return true;
+        //}
+
+        public static bool IsAnagram(string s, string t)
+        {
+            var sdict = new Dictionary<char, int>();
+            var tdict = new Dictionary<char, int>();
+
+            if (s.Length != t.Length)
+                return false;
+            if (s == t)
+                return true;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                sdict[s[i]] = 1 + (sdict.ContainsKey(s[i]) ? sdict[s[i]] : 0);  // if condition is true then the code can be simplified as sdict[s[i]] = 1 +sdict[s[i]]
+                tdict[t[i]] = 1 + (tdict.ContainsKey(t[i]) ? tdict[t[i]] : 0);
+            }
+
+            foreach (var ch in sdict.Keys)
+            {
+                int tCount = tdict.ContainsKey(ch) ? tdict[ch] : 0; 
+
+                if (tCount != sdict[ch])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
 
         public bool ContainsDuplicate(int[] nums)
         {
