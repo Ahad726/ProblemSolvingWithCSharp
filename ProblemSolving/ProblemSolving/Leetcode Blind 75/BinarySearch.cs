1bebc822416f94f8279a6ab0af2fcca21edd531a
Ahad <atmahad726@gmail.com>
2023-02-06 11:33:01 +0600

    Leetcode Find minimum in sorted array

---
+++ ProblemSolving/ProblemSolving/Leetcode Blind 75/BinarySearch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving.Leetcode_Blind_75
+{
+    internal class BinarySearch
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static int FindMin(int[] nums)
+        {
+            int lastIndex = nums.Length - 1;
+            int firstIndex = 0;
+
+
+            if (nums[firstIndex] > nums[lastIndex])
+            {
+                for (int i = lastIndex; i > 0; i--)
+                {
+                    if (nums[i-1] > nums[i])
+                    {
+                        return nums[i];
+                    }
+                }
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
