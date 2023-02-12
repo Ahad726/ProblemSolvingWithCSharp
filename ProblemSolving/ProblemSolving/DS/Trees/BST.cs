d14aa8b123db5fb5c80eaeaec5aa3baea6f7ee38
Ahad <atmahad726@gmail.com>
2023-02-12 17:40:53 +0600

    Leetcode BST Remove and LowestCommonAncestor

---
+++ ProblemSolving/ProblemSolving/DS/Trees/BST.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving.DS.Trees
+{
+    public class BST
+    {
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            var curr = root;
+            while (curr != null)
+            {
+                if (p.val > curr.val && q.val > curr.val)
+                {
+                    curr = curr.right;
+                }
+                else if (p.val < curr.val && q.val < curr.val)
+                {
+                    curr = curr.left;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return curr;
+        }
+
+        public TreeNode FindMinNode(TreeNode root)
+        {
+            var curr = root;
+
+            while (curr != null && curr.left != null)
+            {
+                curr = curr.left;
+            }
+
+            return curr;
+        }
+
+        public TreeNode Remove(TreeNode root, int target)
+        {
+            if (root == null)
+                return null;
+
+             if(target > root.val)
+                root.right = Remove(root.right, target);
+             else if(target < root.val)
+                root.left = Remove(root.left, target);
+            else
+            {
+                if (root.left == null)
+                    return root.right;
+                else if(root.right == null)
+                    return root.left;
+                else
+                {
+                    var minNode = FindMinNode(root.right);
+                    root.val = minNode.val;
+                    root.right = Remove(root.right, minNode.val);
+                }
+            }
+
+            return root;
+
+        }
+
+        public TreeNode Insert(TreeNode root, int val)
+        {
+            if (root == null)
+            {
+                root = new TreeNode(val);
+                return root;
+            }
+
+            if (val > root.val)
+                root.right = Insert(root.right, val);
+            else if (val < root.val)
+                root.left = Insert(root.left, val);
+            return root;
+
+        }
+
+        public bool Search(TreeNode root, int target)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+
+            if (target > root.val)
+            {
+                return Search(root.right, target);
+            }
+            else if (target < root.val)
+            {
+                return Search(root.left, target);
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ ProblemSolving/ProblemSolving/DS/Trees/BSTMain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving.DS.Trees
+{
+    public class BSTMain
+    {
+        static void Main(string[] args)
+        {
+            var bst = new BST();
+
+            var root = bst.Insert(null, 6);
+            bst.Insert(root,2);
+            bst.Insert(root,8);
+            bst.Insert(root,0);
+            bst.Insert(root,4);
+            bst.Insert(root,7);
+            bst.Insert(root,9);
+            bst.Insert(root,3);
+            bst.Insert(root,5);
+
+            root = bst.LowestCommonAncestor(root, new TreeNode(3),new TreeNode(5));
+        }
+    }
+}
