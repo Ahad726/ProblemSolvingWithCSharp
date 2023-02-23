778a6a29db2e9bcd201f04b3abf77ae0e6f9c93c
Ahad <atmahad726@gmail.com>
2023-02-23 23:41:16 +0600

    LeetCode isSameTree

--- ProblemSolving/ProblemSolving/DS/Trees/BSTMain.cs
+++ ProblemSolving/ProblemSolving/DS/Trees/BSTMain.cs
@@ -9,24 +9,29 @@ namespace ProblemSolving.DS.Trees
         static void Main(string[] args)
         {
             var bst = new BST();
-
             var root = bst.Insert(null, 4);
-            bst.Insert(root,3);
-            bst.Insert(root,5);
+            //bst.Insert(root,3);
+            bst.Insert(root,6);
             //bst.Insert(root, 25);
             //bst.Insert(root, 40);
 
+            var bst2 = new BST();
+            var root2 = bst2.Insert(null, 4);
+            //bst2.Insert(root2, 3);
+            bst2.Insert(root2, 5);
 
+
+            Console.WriteLine(bst.IsSameTree(root, root2));
             //var res = bst.MaxDepth(root);
-            bst.Inorder(root);
+            //bst.Inorder(root);
 
             //bst.InvertTree(root);
 
-            Console.WriteLine();
-            bst.PreOrder(root);
+            //Console.WriteLine();
+            //bst.PreOrder(root);
 
-            Console.WriteLine();
-            bst.PostOrder(root);
+            //Console.WriteLine();
+            //bst.PostOrder(root);
 
         }
     }
