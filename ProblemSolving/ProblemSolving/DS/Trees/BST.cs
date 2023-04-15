2b6d3e4f8f9358dadb490b643bfd7b8c1e872362
Ahad <atmahad726@gmail.com>
2023-04-15 15:59:23 +0600

    Building tree from preOrder Inorder

--- ProblemSolving/ProblemSolving/DS/Trees/BST.cs
+++ ProblemSolving/ProblemSolving/DS/Trees/BST.cs
@@ -10,6 +10,31 @@ namespace ProblemSolving.DS.Trees
     public class BST
     {
 
+        public TreeNode BuildTree(int[] preorder, int[] inorder)
+        {
+            return BuildTreeHelper(0, 0, preorder.Length - 1, preorder, inorder);
+        }
+
+        private TreeNode BuildTreeHelper(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder)
+        {
+            if (preorder.Length == 0 && inorder.Length == 0)
+            {
+                return null;
+            }
+
+            if (preStart > preorder.Length - 1 || inStart > inEnd)
+            {
+                return null;
+            }
+
+            var rootNode = new TreeNode(preorder[preStart]);
+            var mid = Array.IndexOf(inorder, preorder[preStart]);
+
+            rootNode.left = BuildTreeHelper(preStart + 1, inStart, mid - 1, preorder, inorder);
+            rootNode.right = BuildTreeHelper(preStart + mid - inStart + 1, mid + 1, inEnd, preorder, inorder);
+            return rootNode;
+        }
+
         public int KthSmallest(TreeNode root, int k)
         {
             var n = 0;
